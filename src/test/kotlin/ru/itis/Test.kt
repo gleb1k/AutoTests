efa2103f60e5c940140f8527caae4c3f49b64f10
gleb <gleb.gafeev@mail.ru>
2024-04-25 15:06:17 +0300

    fix

--- src/test/kotlin/ru/itis/Test.kt
+++ src/test/kotlin/ru/itis/Test.kt
@@ -22,20 +22,25 @@ class Test : BaseTest() {
 
         waitLoading(4)
 
-        loginPage.loginField().sendKeys(LoginPage.EMAIL)
-        waitLoading(2)
-        loginPage.continueButton().click()
-        waitLoading(4)
-        loginPage.passwordField().sendKeys(LoginPage.PASSWORD)
-        waitLoading(2)
-        loginPage.singInButton().click()
-        waitLoading(20)
+        login(LoginPage.EMAIL, LoginPage.PASSWORD)
 
         mainPage.startCreateFolderButton().click()
         waitLoading(4)
         mainPage.nameField().sendKeys(MainPage.FOLDER_NAME + Random.nextInt())
         waitLoading(4)
         mainPage.endCreateFolderButton().click()
+    }
+
+    private fun login(login: String, password: String) {
+        loginPage.loginField().sendKeys(login)
+        waitLoading(2)
+        loginPage.continueButton().click()
+        waitLoading(4)
+        loginPage.passwordField().sendKeys(password)
+        waitLoading(2)
+        loginPage.singInButton().click()
+        waitLoading(20)
+
     }
 
 }
